d1b4b75d455ebfa2b589fda25de6ec1ba18b7c27
Ibrahim Raimi <66981941+ibrahimraimi@users.noreply.github.com>
2022-09-10 00:44:16 +0100

    feat: update title

--- src/Home.tsx
+++ src/Home.tsx
@@ -650,7 +650,7 @@ const Home = (props: HomeProps) => {
                 <MintContainer>
                     <DesContainer>
                         <NFT elevation={3}>
-                            <h2>My NFT</h2>
+                            <h2>Sega</h2>
                             <br/>
                             <div><Price
                                 label={isActive && whitelistEnabled && (whitelistTokenBalance > 0) ? (whitelistPrice + " " + priceLabel) : (price + " " + priceLabel)}/><Image
